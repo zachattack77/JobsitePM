29569d10cbfc494d7551d7cf7c92d2e2fa0775e9
Alex <shornality@yahoo.com>
2018-04-30 17:07:47 -0700

    Home/Login Page setup

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController , NavParams} from 'ionic-angular';
+import {FormBuilder,FormGroup,Validators,AbstractControl} from "@angular/forms";
+import {HttpHeaders, HttpClientModule} from "@angular/common/http";
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data:any = {};
+
+  formgroup:FormGroup;
+  email:AbstractControl;
+  password:AbstractControl;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public formbuilder: FormBuilder,
+              //public http: HttpClientModule
+  ) {
+
+    this.formgroup = this.formbuilder.group({
+      email:['',Validators.required,],
+      password:['',Validators.required]
+    });
+    this.email = this.formgroup.controls['email'];
+    this.password = this.formgroup.controls['password'];
+
 
   }
+
+//functions
+  postData()
+  {
+
+  }
+  // {
+  //   var link = "http://www.pvashchuk.greenriverdev.com/328/jobsite/job.php";
+  //   var myData = JSON.stringify({email: this.data.email});
+  //
+  //   this.http.post(link,myData)
+  //     .subscribe(data=>{
+  //       this.data.response = data["_body"];
+  //       console.log(this.data.response);
+  //     }, error => {
+  //       console.log("Oooops!");
+  //     });
+  // }
 
 }
